68f6faf7eca7666202bd28d5e6192ac10c55ecdf
Alejandro Tafalla <atafalla@bsc.es>
2024-07-11 13:24:06 +0200

    Cleanup and formatting

--- rtl/riscv_dm_wrapper.sv
+++ rtl/riscv_dm_wrapper.sv
@@ -94,122 +94,121 @@ module riscv_dm_wrapper #(
     logic [riscv_dm_pkg::DMI_OP_WIDTH-1:0]      resp_op_cdc;
 
     riscv_dtm dtm(
-        .tms_i(tms_i),
-        .tck_i(tck_i),
-        .trst_i(trst_i),
-        .tdi_i(tdi_i),
-        .tdo_o(tdo_o),
-        .tdo_driven_o(tdo_driven_o),
+        .tms_i          (tms_i),
+        .tck_i          (tck_i),
+        .trst_i         (trst_i),
+        .tdi_i          (tdi_i),
+        .tdo_o          (tdo_o),
+        .tdo_driven_o   (tdo_driven_o),
 
-        .req_valid_o(req_valid),
-        .req_ready_i(req_ready),
-        .req_addr_o(req_addr),
-        .req_data_o(req_data),
-        .req_op_o(req_op),
+        .req_valid_o    (req_valid),
+        .req_ready_i    (req_ready),
+        .req_addr_o     (req_addr),
+        .req_data_o     (req_data),
+        .req_op_o       (req_op),
 
-        .resp_valid_i(resp_valid_cdc),
-        .resp_ready_o(resp_ready_cdc),
-        .resp_data_i(resp_data_cdc),
-        .resp_op_i(resp_op_cdc)
+        .resp_valid_i   (resp_valid_cdc),
+        .resp_ready_o   (resp_ready_cdc),
+        .resp_data_i    (resp_data_cdc),
+        .resp_op_i      (resp_op_cdc)
     );
 
     cdc_2phase_clearable #(
-        .T(logic [riscv_dm_pkg::DMI_ADDR_WIDTH+riscv_dm_pkg::DMI_DATA_WIDTH+riscv_dm_pkg::DMI_OP_WIDTH-1:0])
+        .T                      (logic [riscv_dm_pkg::DMI_ADDR_WIDTH+riscv_dm_pkg::DMI_DATA_WIDTH+riscv_dm_pkg::DMI_OP_WIDTH-1:0])
     ) req_cdc (
-        .src_rst_ni(~trst_i),
-        .src_clk_i(tck_i),
-        .src_clear_i(0),
-        .src_clear_pending_o(),
-        .src_data_i({req_addr, req_data, req_op}),
-        .src_valid_i(req_valid),
-        .src_ready_o(req_ready),
+        .src_rst_ni             (~trst_i),
+        .src_clk_i              (tck_i),
+        .src_clear_i            (0),
+        .src_clear_pending_o    (),
+        .src_data_i             ({req_addr, req_data, req_op}),
+        .src_valid_i            (req_valid),
+        .src_ready_o            (req_ready),
 
-        .dst_rst_ni(rstn_i),
-        .dst_clk_i(clk_i),
-        .dst_clear_i(0),
-        .dst_clear_pending_o(),
-        .dst_data_o({req_addr_cdc, req_data_cdc, req_op_cdc}),
-        .dst_valid_o(req_valid_cdc),
-        .dst_ready_i(req_ready_cdc)
+        .dst_rst_ni             (rstn_i),
+        .dst_clk_i              (clk_i),
+        .dst_clear_i            (0),
+        .dst_clear_pending_o    (),
+        .dst_data_o             ({req_addr_cdc, req_data_cdc, req_op_cdc}),
+        .dst_valid_o            (req_valid_cdc),
+        .dst_ready_i            (req_ready_cdc)
     );
 
     cdc_2phase_clearable #(
         .T(logic [riscv_dm_pkg::DMI_DATA_WIDTH+riscv_dm_pkg::DMI_OP_WIDTH-1:0])
     ) resp_cdc (
-        .src_rst_ni(rstn_i),
-        .src_clk_i(clk_i),
-        .src_clear_i(0),
-        .src_clear_pending_o(),
-        .src_data_i({resp_data, resp_op}),
-        .src_valid_i(resp_valid),
-        .src_ready_o(resp_ready),
+        .src_rst_ni             (rstn_i),
+        .src_clk_i              (clk_i),
+        .src_clear_i            (0),
+        .src_clear_pending_o    (),
+        .src_data_i             ({resp_data, resp_op}),
+        .src_valid_i            (resp_valid),
+        .src_ready_o            (resp_ready),
 
-        .dst_rst_ni(~trst_i),
-        .dst_clk_i(tck_i),
-        .dst_clear_i(0),
-        .dst_clear_pending_o(),
-        .dst_data_o({resp_data_cdc, resp_op_cdc}),
-        .dst_valid_o(resp_valid_cdc),
-        .dst_ready_i(resp_ready_cdc)
+        .dst_rst_ni             (~trst_i),
+        .dst_clk_i              (tck_i),
+        .dst_clear_i            (0),
+        .dst_clear_pending_o    (),
+        .dst_data_o             ({resp_data_cdc, resp_op_cdc}),
+        .dst_valid_o            (resp_valid_cdc),
+        .dst_ready_i            (resp_ready_cdc)
     );
 
     riscv_dm #(
-        .NUM_HARTS(NUM_HARTS),
-        .PROGRAM_SIZE(PROGRAM_SIZE),
-        .DATA_SIZE(DATA_SIZE),
-        .WORD_SIZE(WORD_SIZE),
-        .NUM_PHYS_REGS(NUM_PHYS_REGS)
+        .NUM_HARTS          (NUM_HARTS),
+        .PROGRAM_SIZE       (PROGRAM_SIZE),
+        .DATA_SIZE          (DATA_SIZE),
+        .WORD_SIZE          (WORD_SIZE),
+        .NUM_PHYS_REGS      (NUM_PHYS_REGS)
     ) dm(
-        .clk_i(clk_i),
-        .rstn_i(rstn_i),
+        .clk_i              (clk_i),
+        .rstn_i             (rstn_i),
 
-        .req_valid_i(req_valid_cdc),
-        .req_ready_o(req_ready_cdc),
-        .req_addr_i(req_addr_cdc),
-        .req_data_i(req_data_cdc),
-        .req_op_i(req_op_cdc),
+        .req_valid_i        (req_valid_cdc),
+        .req_ready_o        (req_ready_cdc),
+        .req_addr_i         (req_addr_cdc),
+        .req_data_i         (req_data_cdc),
+        .req_op_i           (req_op_cdc),
 
-        .resp_valid_o(resp_valid),
-        .resp_ready_i(resp_ready),
-        .resp_data_o(resp_data),
-        .resp_op_o(resp_op),
+        .resp_valid_o       (resp_valid),
+        .resp_ready_i       (resp_ready),
+        .resp_data_o        (resp_data),
+        .resp_op_o          (resp_op),
 
-        .resume_request_o(resume_request_o),
-        .resume_ack_i(resume_ack_i),
-        .running_i(running_i),
+        .resume_request_o   (resume_request_o),
+        .resume_ack_i       (resume_ack_i),
+        .running_i          (running_i),
 
-        .halt_request_o(halt_request_o),
-        .halted_i(halted_i),
+        .halt_request_o     (halt_request_o),
+        .halted_i           (halted_i),
 
-        .progbuf_run_req_o(progbuf_run_req_o),
-        .progbuf_run_ack_i(progbuf_run_ack_i),
-        .parked_i(parked_i),
+        .progbuf_run_req_o  (progbuf_run_req_o),
+        .progbuf_run_ack_i  (progbuf_run_ack_i),
+        .parked_i           (parked_i),
 
-        .halt_on_reset_o(halt_on_reset_o),
-        .hart_reset_o(hart_reset_o),
+        .halt_on_reset_o    (halt_on_reset_o),
+        .hart_reset_o       (hart_reset_o),
 
-        .havereset_i(havereset_i),
-        .unavail_i(unavail_i),
+        .havereset_i        (havereset_i),
+        .unavail_i          (unavail_i),
 
-        .rnm_read_en_o(rnm_read_en_o),
-        .rnm_read_reg_o(rnm_read_reg_o),
-        .rnm_read_resp_i(rnm_read_resp_i),
+        .rnm_read_en_o      (rnm_read_en_o),
+        .rnm_read_reg_o     (rnm_read_reg_o),
+        .rnm_read_resp_i    (rnm_read_resp_i),
 
-        .rf_en_o(rf_en_o),
-        .rf_preg_o(rf_preg_o),
-        .rf_rdata_i(rf_rdata_i),
+        .rf_en_o            (rf_en_o),
+        .rf_preg_o          (rf_preg_o),
+        .rf_rdata_i         (rf_rdata_i),
 
-        .rf_we_o(rf_we_o),
-        .rf_wdata_o(rf_wdata_o),
+        .rf_we_o            (rf_we_o),
+        .rf_wdata_o         (rf_wdata_o),
 
 
-        .sri_addr_i(sri_addr),
-        .sri_en_i(sri_en),
-        .sri_wdata_i(sri_wdata),
-        .sri_we_i(sri_we),
-        .sri_be_i(sri_be),
-        .sri_rdata_o(sri_rdata),
-        .sri_error_o(sri_error)
-
+        .sri_addr_i         (sri_addr_i),
+        .sri_en_i           (sri_en_i),
+        .sri_wdata_i        (sri_wdata_i),
+        .sri_we_i           (sri_we_i),
+        .sri_be_i           (sri_be_i),
+        .sri_rdata_o        (sri_rdata_o),
+        .sri_error_o        (sri_error_o)
     );
 endmodule
